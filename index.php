7d9736a9f4412faa145effad0d2e4230db1897b6
andre-amad <amadandre8@gmail.com>
2026-01-05 08:13:30 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>hello world</h1>
+
+    <?php
+     for($x = 0; $x <10; $x++){
+       echo("<h1> hello world from php  <br> </h1> ");
+
+       
+    }
+    ?>
+    <
+
+
+</body>
+</html>
