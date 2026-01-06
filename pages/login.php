f21db7cb9ea2a1292be7eb7d7f3224352859d439
andre-amad <amadandre8@gmail.com>
2026-01-06 08:51:42 +0800

    new changes

--- pages/login.php
+++ pages/login.php
@@ -4,15 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title> 
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>login</h1>
+    <div class="logincard">
     <form action="">
-        <label for ="username">Username</label>
-        <input type ="text" id="username" class="username" name="username">
-          <label for ="password">password</label>
-          <input type ="password" id="password" class="password" name="password">
-          <button type="sumbit">Amad enter</button>
+        <label for ="username">Username</label><br>
+        <input type ="text" id="username" class="username" name="username"><br>
+          <label for ="password">password</label><br>
+          <input type ="password" id="password" class="password" name="password"> <br>
+          <button type="sumbit">Amad enter</button> <br>
     </form>
+    </div>
+
+<?php
+        $name = "Andre";
+        $nums = "1";
+        $num  =  1;
+
+     echo("hello word/nt" . $name);
+     echo($num + $nums);
+
+     var_dump($nums);
+     var_dump($num);
+
+
+     
+
+?>
+
+
+
 </body>
 </html>
